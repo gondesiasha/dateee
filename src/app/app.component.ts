a21a362e49d861b762ff4f6fa4a3cdfff3eb8991
gondesiasha <50900238+gondesiasha@users.noreply.github.com>
2019-07-09 00:21:27 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+//import { Component } from '@angular/core';
+
+import { Component,OnInit } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit  {
+  title = 'exercise2';
+  constructor(private http:HttpClient)
+  {  }
+
+ 
+  ngOnInit(){
+    
+
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+     FormsModule ,    
+    HttpClientModule, BsDatepickerModule.forRoot(), BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
